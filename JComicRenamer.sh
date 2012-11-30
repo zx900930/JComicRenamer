626e6056ad77825d9236b120a3381620b564e337
zx900930 <zx900930@gmail.com>
2012-11-30 07:29:20 +0100

    new file:   JComicRenamer.sh

---
+++ JComicRenamer.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "Must stop JComicDowloader first, stop now?(Y/N): "
+read answer
+echo
+if [ "$answer" = "n" ] || [ "$answer" = "N" ]
+then exit
+fi
+if [ "$answer" != "y" -a "$answer" != "Y" ]
+then
+echo
+echo "Response $answer is not recognized, "
+echo
+echo "Exiting script..."
+echo
+exit 1
+fi
+
+pkill java
+echo "Stoping JComicDowloader..."
+
+cd $HOME/Images/Comics/
+ls -rt | tac | grep 'JComicDownloader_v.*' -m 1 | read newapp
+
+if [[ ! -f $newapp ]]
+then
+echo
+echo "Could not find the new JComicDowloader program, please download it first!"
+echo
+echo "Exiting script..."
+echo
+exit 1
+fi
+
+echo
+echo "Find new $newapp"
+echo
+mv $newapp JComicDownloader.jar
+echo
+echo "The new $newapp has been renamed to JComicDownloader.jar"
+echo
+
+ls -rt | grep 'JComicDownloader.*' -m 1 | read oldapp
+
+if [[ ! -f $oldapp ]]
+then
+echo
+echo "Could not find the old JComicDowloader program"
+echo
+echo "Exiting script..."
+echo
+exit 1
+fi
+
+echo
+echo "Find old $oldapp"
+echo
+rm $oldapp
+echo
+echo "Old version：$oldapp has been removed."
+echo
+exit 0
